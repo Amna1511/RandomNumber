abf60e235ab67412e534d833a3291e1bad0a3bd0
amna1511 <amna.13aug@gmail.com>
2020-01-24 14:04:36 +0100

    First commit

---
+++ JavaScript.js
@@ -0,0 +1,20 @@
+﻿
+  function myFunction() {
+      var x = document.getElementById("myNumber").value;
+      document.getElementById("demo").innerHTML = x;
+
+    }
+
+function myFunction1() {
+        var y = Math.floor((Math.random() * 10) + 1);
+    document.getElementById("demo1").innerHTML = y;
+
+}
+
+function myFunction2(x,y) {
+ 
+    var z = (document.getElementById("demo").innerHTML == document.getElementById("demo1").innerHTML) ? "Congratulations" : "sorry";
+    document.getElementById("demo2").innerHTML = z;
+
+}
+    
